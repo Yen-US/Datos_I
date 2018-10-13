a28790dc1e36b3d20f7d0718e64eb85e6df03c3f
Yenson Umaña <yen21000@gmail.com>
2018-10-12 19:43:44 -0600

    Se actualiza la aplicación de android List_Sort

--- ListSort/app/src/main/java/com/yenus210/listsort/MainScreen.java
+++ ListSort/app/src/main/java/com/yenus210/listsort/MainScreen.java
@@ -8,21 +8,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.List;
-
 public class MainScreen extends AppCompatActivity {
 
     EditText dato, n;
     TextView lOrd;
     Button bubble, shell, add;
     ImageView gráficas;
+
+    int len, n_dato, largo=0;
     int lista_A[];
-    String lista_P;
-    int len, n_dato, i=0;
+
 
     //ShellSort
     public void sort(int arr[]) {
         int n = arr.length;
+        String lista_P="";
         for (int gap = n / 2; gap > 0; gap /= 2) {
             for (int i = gap; i < n; i += 1) {
                 int temp = arr[i];
@@ -33,16 +33,44 @@ public class MainScreen extends AppCompatActivity {
 
             }
 
+        }for (int i = 0; i < largo; i++) {
+            if (i!=largo-1) {
+                lista_P += (String.valueOf(lista_A[i]) + ",");
+            }else{
+                lista_P += (String.valueOf(lista_A[i]));
+                lOrd.setText(lista_P);
+            }
 
         }
-        for (int i = 0; i < n; i++) {
-            lista_P.concat(String.valueOf(arr[i]) + ",");
-            lOrd.setText(lista_P);
+        }
+
+
+    //Bubble Sort
+    void bubble_srt(int array[]) {
+        int n = array.length;
+        int k;
+        String lista_P="";
+        for (int m = 0; m < n; m++) {
+            for (int i = 0; i < n - 1; i++) {
+                k = i + 1;
+                if (array[i] > array[k]) {
+                    int temp;
+                    temp = array[i];
+                    array[i] = array[k];
+                    array[k] = temp;
+                }
+            }// aqui se imprime el proceso del sort
+
+        }
+        for (int i = 0; i < largo; i++) {
+            if (i != largo - 1) {
+                lista_P += (String.valueOf(lista_A[i]) + ",");
+            } else {
+                lista_P += (String.valueOf(lista_A[i]));
+                lOrd.setText(lista_P);
+            }
         }
     }
-    //Bubble Sort
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,29 +82,45 @@ public class MainScreen extends AppCompatActivity {
         shell = (Button)findViewById(R.id.shell);
         gráficas = (ImageView)findViewById(R.id.gráficas);
         add=(Button)findViewById(R.id.add);
-        n=(EditText)findViewById(R.id.n);
+        n=(EditText) findViewById(R.id.n);
 
         add.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                len=Integer.parseInt(String.valueOf(n.getText()));
-                n_dato=Integer.parseInt(String.valueOf(dato.getText()));
-                if (i==0){
+                len=Integer.parseInt(n.getText().toString());
+                n_dato=Integer.parseInt(dato.getText().toString());
+                if (largo==0){
                     lista_A = new int[len];
-                    lista_A[i]=n_dato;
-                    i++;
-                }else if (i>0 && i<len) {
-                    lista_A[i] = n_dato;
-                    i++;
-                }else if (i==len) {
-                    lOrd.setText("Escoja el ordenamiento");
+                    lista_A[largo] = n_dato;
+                    lOrd.setText(String.valueOf(lista_A[largo] + ","));
+                    largo+=1;
+
+                }else if (largo>0 && largo<len) {
+                    lista_A[largo]= n_dato;
+                    lOrd.setText(String.valueOf(lista_A[largo] + ","));
+                    largo+=1;
+
+                }if (largo==len||largo>len) {
+                    lOrd.setText(String.valueOf(lista_A[largo-1] + ","));
+                    lOrd.setText("Escoja el ordenamieno");
+
                 }
 
             }
         });
         bubble.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-
-
+                bubble_srt(lista_A);
+                gráficas.setImageResource(R.mipmap.sort_base);
+                len=Integer.parseInt(n.getText().toString());
+                    if (len <=5){
+                        gráficas.setImageResource(R.mipmap.sort_5);
+                    }else if(len>5&&len<=10){
+                        gráficas.setImageResource(R.mipmap.sort_10);
+                    }else if(len>10&&len<=25){
+                        gráficas.setImageResource(R.mipmap.sort_25);
+                    }else if(len>25){
+                        gráficas.setImageResource(R.mipmap.sort_30);
+                    }
             }
         });
 
@@ -85,5 +129,6 @@ public class MainScreen extends AppCompatActivity {
                 sort(lista_A);
             }
         });
+
     }
 }
